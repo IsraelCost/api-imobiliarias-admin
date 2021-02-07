4d3ef66e72b06076c576b49beb4617cf72a47f29
IsraelCost <cmteisrael@gmail.com>
2021-02-07 13:23:45 -0300

    alterando rota da api

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { getToken } from './auth';
 
 const api = axios.create({
-  baseURL: 'https://api.houzes.com.br/'
+  baseURL: 'https://app.houzes.com.br/'
 });
 
 api.interceptors.request.use(async config => {
